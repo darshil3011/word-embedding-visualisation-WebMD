3e0c6573a5daaa33fcae9c8d269a6d8f0b757189
Darshil Modi <45987056+darshil3011@users.noreply.github.com>
2023-06-06 09:59:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ fig = px.timeline(df_name, x_start="start", x_end="end", y="Doctor")
 st.plotly_chart(fig)
 
 st.header("Precaution better than cure")
-url = 'https://stackoverflow.com'
+url = 'https://bhavnasjain.github.io/data-visualization/'
 
 st.markdown(f'''
 <a href={url}><button style="background-color:GreenYellow;">Stay Fit !</button></a>
